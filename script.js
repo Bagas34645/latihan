5da2fc703b0e06a3ab4f15a57098191f5298422a
Bagas Abiyu Kumara <149683919+Bagas34645@users.noreply.github.com>
2024-11-15 18:55:51 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+// Mengatur menu toggle
 const menuIcon = document.getElementById("menu-icon");
 const menuList = document.getElementById("menu-list");
 
 menuIcon.addEventListener("click", () => {
   menuList.classList.toggle("hidden");
+});
+
+// Menangani pengiriman formulir
+const contactForm = document.getElementById("contactForm");
+
+contactForm.addEventListener("submit", (event) => {
+  event.preventDefault(); // Mencegah reload halaman
+  alert("Berhasil dikirim!"); // Menampilkan alert
+  contactForm.reset(); // Reset form
 });
